be7f502a0fe1cb289e3c98d43342304fd69188b1
leonardo.paixao <leonardorpaixao@gmail.com>
2020-03-29 02:41:09 -0300

    fix: app repo

---
+++ mobile-app/src/screens/Detail/index.js
@@ -0,0 +1,10 @@
+//import liraries
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+// create a component
+export default function Detail(){
+    return (
+        <View></View>
+    );
+};
